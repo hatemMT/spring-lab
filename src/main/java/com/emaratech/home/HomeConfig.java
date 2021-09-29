5e437ef047b921e56b88095642272cef710cd2a4
Hatem <hatem.hm.7@gmail.com>
2021-09-29 21:32:55 +0200

    Loading properties according to active profile

--- src/main/java/com/emaratech/home/HomeConfig.java
+++ src/main/java/com/emaratech/home/HomeConfig.java
@@ -1,13 +1,13 @@
 package com.emaratech.home;
 
-import com.emaratech.home.control.HomeRepository;
 import com.emaratech.home.boundary.HomeService;
+import com.emaratech.home.control.HomeRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:/application.properties")
+@PropertySource("classpath:/application-${spring.profiles.active}.properties")
 public class HomeConfig {
 
 
